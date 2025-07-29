9efb0c8cd7b1e30b27b8e791b8d391c4b7200c85
Pratik17-sudo <pratikdahat09@gmail.com>
2025-07-30 02:42:55 +0530

    final Commit Ig

--- api/app.js
+++ api/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import authRoute from "./routes/auth.route.js";
-import postRoute from "./routes/post.route.js"; 
+import postRoute from "./routes/post.route.js";
 import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
 import chatRoute from "./routes/chat.route.js";
@@ -15,15 +15,15 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({origin:process.env.CLIENT_URL, credentials: true}));
-app.use(express.json());  
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(express.json());
 app.use(cookieParser());
 
 
 app.use("/api/auth", authRoute);
-app.use("/api/users",userRoute);
-app.use("/api/posts", postRoute); 
-app.use("/api/test", testRoute); 
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/test", testRoute);
 app.use("/api/chats", chatRoute);
 app.use("/api/messages", messageRoute);
 
@@ -32,6 +32,11 @@ app.use("/api/test", (req, res) => {
     res.send("It works");
 });
 
-app.listen(8800, () => {
-    console.log("Server is running on port 8800");
-});
+if (process.env.NODE_ENV !== "production") {
+    app.listen(8800, () => {
+        console.log("Server running on port 8800");
+    });
+}
+
+export default app;
+
